d56e84f0a17ea57500227a2c59eb4c240a2e6b37
Jaslin <garciamj@umich.edu>
2026-09-21 11:21:41 -0400

    Update package.json and App.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+} from 'week-3-inclass';
 
 export default function App() {
   return (
