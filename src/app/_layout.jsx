eecf58f6c4b50b902c798ef38fc68d6000256a5f
김동섭 <dongsubk@umich.edu>
2026-09-26 15:06:45 -0400

    feat: use safearea in global layout

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,11 +1,14 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="dark" />
-      <Stack screenOptions={{ headerShown: false }} />
+      <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
     </>
   );
 }
